d7e5d0d26d15b23f6fd7f3e8e0594e77b7841fc8
Haru Im <haru@Haruui-MacBookAir.local>
2023-08-05 19:36:41 +0900

    fix lintrc + set up prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,23 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "plugin:react/recommended"],
-  parser: "@typescript-eslint/parser",
+  extends: ['next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
-  ignorePatterns: ["node_modules", "dist"],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-    "unused-imports",
-    "react-hooks",
-    "unused-imports",
-  ],
+  ignorePatterns: ['node_modules', 'dist'],
+  plugins: ['@typescript-eslint', 'import', 'unused-imports'],
   rules: {
-    "unused-imports/no-unused-imports-ts": "error",
-    "react/react-in-jsx-scope": "off",
-    "unused-imports/no-unused-imports-ts": "error",
+    'unused-imports/no-unused-imports-ts': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'unused-imports/no-unused-imports-ts': 'error',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
